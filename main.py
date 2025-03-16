ecb3b17bf9fa8624794162d71a1286f80966bf8f
manuleao <manoela.matos001@edu.pucrs.br>
2025-03-16 22:54:58 +0000

    Com def calcular_ano_mais_lancamentos definida

--- main.py
+++ main.py
@@ -1,57 +1,48 @@
-
-f = open("games_amostra.csv")
+class Jogos:
+  def __init__(self, arquivo):
+    self.arquivo = arquivo
+    self.jogos = []
+  def Abrir_Arquivo(self):
+    try:
+      with open(self.arquivo, 'r', encoding='utf-8') as f:
+        primeira = True
+        for linha in f:
+          if primeira:
+           primeira = False
+           continue
+          self.jogos.append(linha.strip().split(';'))
+    except FileNotFoundError:
+      print(f"Erro: O arquivo '{self.arquivo}' não foi encontrado.")
+    except Exception as e:
+      print(f"Erro ao abrir o arquivo: {e}")
 
 ######### ano #
-lista_ano = []
-lista_repeticao = []
-lista_ano_repeticao = []
+  def calcular_ano_mais_lancamentos(self):
+    lista_ano = []
+    lista_repeticao = []
+    lista_ano_repeticao = []
+    for jogo in self.jogos:
+      ano_jogo = jogo[2][:4]
+      lista_repeticao.append(ano_jogo) 
+      if ano_jogo not in lista_ano:
+        lista_ano.append(ano_jogo)
+    for x in lista_ano:
+      lista_ano_repeticao.append((lista_repeticao.count(x)))
+    anos_indices = [i for i, valor in enumerate(lista_ano_repeticao) if valor == max(lista_ano_repeticao)]
+    anos_com_mais_lancamentos = [lista_ano[i] for i in anos_indices]
+    if len(anos_com_mais_lancamentos) > 1:
+      return f"Anos com mais lançamentos: {', '.join(anos_com_mais_lancamentos)}"
+    if len(anos_com_mais_lancamentos) == 1:
+      return f"Ano com mais lançamentos: {anos_com_mais_lancamentos[0]}"
+    
+  def exibir_resultados_ano(self):
+    ano_com_mais_lancamentos = self.calcular_ano_mais_lancamentos()
+    print(ano_com_mais_lancamentos)
+    return ano_com_mais_lancamentos
 
 
-gratuitos = 0
-pagos = 0
+if __name__ == "__main__":
+  visao = Jogos("games_amostra.csv")
+  visao.Abrir_Arquivo()
+  visao.exibir_resultados_ano()
 
-jogos_menosdez_saldo_avpositivo = 0
-
-primeira = True
-for linha in f:
-  if primeira:
-    primeira = False
-    continue
-  lista = linha.strip().split(";")
-  if int(lista[6]) < 1000 and int(lista[22]) > int(lista[23]):
-    jogos_menosdez_saldo_avpositivo += 1
-  ######### perecentual de jogos gratuitos e pagos na plataforma #########
-
-
-  if lista[6] == "0":
-    gratuitos += 1
-  if lista[6] != "0":
-    pagos += 1
-
-
-######### ano com mais jogos novos #########
-  
-  if lista_ano.count(lista[2][:4]) == 0:
-          lista_ano.append(lista[2][:4])
-  if lista_ano.count(lista[2][:4]) > 0:
-          lista_repeticao.append(linha.strip().split(';')[2][:4])
-
-
-for l in lista_ano:
-  lista_ano_repeticao.append((lista_repeticao.count(l)))
-  
-
-#print(lista_ano)
-#print(lista_repeticao)
-#print(lista_ano_repeticao)
-print('O ano com maior número de jogos novos é: ', lista_ano[(lista_ano_repeticao.index(max(lista_ano_repeticao)))])
-
-######### perecentual de jogos gratuitos e pagos na plataforma #########
-
-print('Percentual de jogos gratuitos: ', gratuitos/(gratuitos+pagos)*100, '%')
-print('Percentual de jogos pagos: ', pagos/(gratuitos+pagos)*100, '%')
-
-
-######### jogos de menos de 10 dolares com mais avaliacao positiva que negativa #########
-
-print('Quantidade de jogos que custam menos de 10 dólares e que possuem mais avaliações positivas que negativas: ', jogos_menosdez_saldo_avpositivo)
